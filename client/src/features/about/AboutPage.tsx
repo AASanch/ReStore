7dfce8815bba0c109add4bb190105ccc630dfef9
Adrian Sanchez <adrian.sanchez@emerson.com>
2024-05-15 14:47:59 -0500

    Added test page for error handling.  Introduced "agent" for consolidating api calls.

--- client/src/features/about/AboutPage.tsx
+++ client/src/features/about/AboutPage.tsx
@@ -1,5 +1,47 @@
-import { Typography } from "@mui/material";
+import { Button, ButtonGroup, Container, Typography } from "@mui/material";
+import agent from "../../app/api/agent";
 
 export default function AboutPage() {
-  return <Typography variant="h2">About page</Typography>;
+  return (
+    <Container>
+      <Typography gutterBottom variant="h2">
+        Errors for testing purposes
+      </Typography>
+      ;
+      <ButtonGroup fullWidth>
+        <Button
+          variant="contained"
+          onClick={() => agent.TestErrors.get400Error().catch(console.log)}
+        >
+          Test 400 Error
+        </Button>
+        <Button
+          variant="contained"
+          onClick={() => agent.TestErrors.get401Error().catch(console.log)}
+        >
+          Test 401 Error
+        </Button>
+        <Button
+          variant="contained"
+          onClick={() => agent.TestErrors.get404Error().catch(console.log)}
+        >
+          Test 404 Error
+        </Button>
+        <Button
+          variant="contained"
+          onClick={() => agent.TestErrors.get500Error().catch(console.log)}
+        >
+          Test 500 Error
+        </Button>
+        <Button
+          variant="contained"
+          onClick={() =>
+            agent.TestErrors.getValidationError().catch(console.log)
+          }
+        >
+          Test Validation Error
+        </Button>
+      </ButtonGroup>
+    </Container>
+  );
 }
